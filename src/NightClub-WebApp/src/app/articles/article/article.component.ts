7c37781111c739d3bbc1c37b20f9af5869686612
Petar Perković <petar.perkovic15@gmail.com>
2021-06-08 13:47:43 +0200

    8.6.2021

--- src/NightClub-WebApp/src/app/articles/article/article.component.ts
+++ src/NightClub-WebApp/src/app/articles/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/_models/Article';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-
+  article: Article = {
+    id: 0,
+    title: '',
+    content: '',
+    publishingDate: null,
+    photoFilePath: '',
+    photoURL: '#'
+  }
+  fileName: string = 'Choose image';
+  fileBtn: string = this.fileName;
   constructor() { }
 
   ngOnInit() {
   }
 
+  uploadPhoto(files) {
+    var reader = new FileReader();
+    this.fileName = files[0].name;
+    this.fileBtn = 'Replace image'
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.article.photoURL = reader.result;
+    }
+  }
 }
