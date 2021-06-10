49a96812ecd0be3924fbb35076bca87aba7d0e4b
Petar Perković <petar.perkovic15@gmail.com>
2021-06-10 11:29:45 +0200

    10.6.2021

--- src/NightClub-WebApp/src/app/home/home.component.ts
+++ src/NightClub-WebApp/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { ArticleService } from './../_services/article.service';
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../_models/Article';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  articles: Article[];
+  constructor(private articleService: ArticleService) { }
 
   ngOnInit() {
+    this.articleService.getArticles()
+      .subscribe(a => {
+        this.articles = a.slice(0, 3);
+      });
   }
 
 }
